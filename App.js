ec0c3bc0d41c1f1995aba96024a7e809242e7d94
Nata Vacheishvili <natashkela@gmail.com>
2023-04-30 20:08:30 -0400

    custom font installed

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
 
 const App = () => {
-  return null;
+  return (
+    <SafeAreaView>
+      <Text style={[]}>Hello World!</Text>
+    </SafeAreaView>
+  );
 };
 
 export default App;
